d4dea12367f86ec1e92a14609d2805e785c07ed6
Shubham Jain <jainshubhamsunil@gmail.com>
2019-10-04 13:16:20 -0400

    Implement saving maps to .bmp, and section off save functions

--- mapgen.cpp
+++ mapgen.cpp
@@ -8,7 +8,6 @@
 
 #include "perlin.hpp"
 
-
     //grayscale needs [0, 1]
     //perlVal = (perlVal + 1) / 2;
     //For grayscale
@@ -17,6 +16,8 @@
     //int r = b * 0x10000;
     //finalVal = r + g + b;
 
+void saveMapPNG(SDL_Renderer*, int, int);
+void saveMap(double[], int, int);
 
 using namespace std;
 
@@ -55,13 +56,13 @@ int main(int argc, char** argv) {
   //Lower persistance results in more blobby, less detail maps
   //something that looks like singular continents
   //Higher persistance results in MASSIVE world views, with high
-  //detail resolution
-  //Amplitude(wrt p) controls the upper bound and lower bounds of the map
-  //freq controls the start frequency, start at lower values, and it keeps doubling
+  //detail resolution 
+  //Amplitude(wrt p) controls the upper bound and lower bounds of the map 
+  //freq controls the start frequency, start at lower values, and it keeps doubling 
+
   generateMap(width, height, 128, 0.6, 1, 3, 8, map1);
 
-  int space = width * height;
-
+  int space = width * height; 
   double max = -1;
   double min = 1;
   for(int i = 0; i < space; i++) {
@@ -69,8 +70,6 @@ int main(int argc, char** argv) {
     if(mapFinal[i] > max) max = mapFinal[i];
     if(mapFinal[i] < min) min = mapFinal[i];
   }
-
-  printf("Max: %f\nMin: %f\n", max, min);
 
   //Worth a try
   //Normalize all points
@@ -81,39 +80,9 @@ int main(int argc, char** argv) {
     if(mapFinal[i] > newmax) newmax = mapFinal[i];
     if(mapFinal[i] < newmin) newmin = mapFinal[i];
   }
+  printf("Data normalized\n");
 
-  printf("After normalization (expected: [0,1])\n");
-  printf("NewMax: %f\nNewMin: %f\n", newmax, newmin);
-
-  ////////////////////////////////////////////////////////
-
-  //saving values in file for analytics
-  time_t cur_time = time(0);
-  char* time_as_str = ctime(&cur_time);
-  time_as_str[strlen(time_as_str) - 2] = 0;
-
-  string loc("maps/Map_");
-  string fileName(time_as_str);
-  string::iterator end_pos = remove(fileName.begin(), fileName.end(), ' ');
-  fileName.erase(end_pos, fileName.end());
-  string filetype(".txt");
-  fileName = loc + fileName + filetype;
-  
-  printf("Writing data to %s\n", fileName.c_str());
-  FILE* mapVals = fopen(fileName.c_str(), "w");
-  if(!mapVals) {
-    printf("File creation failed\n");
-    return 1;
-  }
-
-  for(int i = 0; i < space - 1; i++) {
-    fprintf(mapVals, "%.2f,", mapFinal[i]); 
-  }
-  fprintf(mapVals, "%.2f", mapFinal[space - 1]);
-  fclose(mapVals);
-  mapVals = NULL;
-
-  ////////////////////////////////////////////////////////
+  saveMap(mapFinal, width, height);
 
   double maxValue = 1.00f;
 
@@ -122,14 +91,14 @@ int main(int argc, char** argv) {
   int coastal_water = 0x0077be;
   int beach_sand = 0xeed6af;
   int inner_sand = 0xd2b55b;
-  int lowland_green = 0x608038;    //0x7cfc00;
+  int lowland_green = 0x608038;
   int midland_green = 0x4a6904;
   int low_rocky = 0x977c53;
   int rocky = 0x796342;
   int mountainous = 0x9e9e9e;
   int snowy = 0xf0f0ec;
   
-  for(int i = 0; i < width * height; i++) {
+  for(int i = 0; i < space; i++) {
     int finalVal = 0;
 
     if(mapFinal[i] < 0.32 * maxValue) finalVal = deep_ocean;
@@ -151,6 +120,9 @@ int main(int argc, char** argv) {
 	SDL_RenderClear(renderer);
 	SDL_RenderCopy(renderer, texture, NULL, NULL);
   SDL_RenderPresent(renderer);
+
+  //save png
+  saveMapPNG(renderer, width, height);
   
   bool quit = false;
 
@@ -172,3 +144,54 @@ int main(int argc, char** argv) {
 
   return 0;
 }
+
+void saveMapPNG(SDL_Renderer* renderer, int width, int height) {
+  SDL_Surface* ss = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0); 
+  SDL_RenderReadPixels(renderer, NULL, ss->format->format, ss->pixels, ss->pitch);
+
+  time_t cur_time = time(0);
+  char* time_as_str = ctime(&cur_time);
+  time_as_str[strlen(time_as_str) - 2] = 0;
+
+  string loc("images/Map_");
+  string fileName(time_as_str);
+  string::iterator end_pos = remove(fileName.begin(), fileName.end(), ' ');
+  fileName.erase(end_pos, fileName.end());
+  string filetype(".bmp");
+  fileName = loc + fileName + filetype;
+
+  SDL_SaveBMP(ss, fileName.c_str());
+  SDL_FreeSurface(ss);
+}
+
+void saveMap(double points[], int width, int height) {
+  long space = width * height;
+
+  time_t cur_time = time(0);
+  char* time_as_str = ctime(&cur_time);
+  time_as_str[strlen(time_as_str) - 2] = 0;
+
+  string loc("maps/Map_");
+  string fileName(time_as_str);
+  string::iterator end_pos = remove(fileName.begin(), fileName.end(), ' ');
+  fileName.erase(end_pos, fileName.end());
+  string filetype(".txt");
+  fileName = loc + fileName + filetype;
+  
+  printf("Writing data to %s\n", fileName.c_str());
+  FILE* mapVals = fopen(fileName.c_str(), "w");
+  if(!mapVals) {
+    printf("File creation failed\n");
+    SDL_Quit();
+  }
+
+  for(int i = 0; i < space - 1; i++) {
+    fprintf(mapVals, "%.2f,", points[i]); 
+  }
+
+  fprintf(mapVals, "%.2f", points[space - 1]);
+  fclose(mapVals);
+  mapVals = NULL;
+}
+
+
