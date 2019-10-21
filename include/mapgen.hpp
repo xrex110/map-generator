7fb000c505b037cb3f2c51ebdb410200b6889026
Shubham Jain <jainshubhamsunil@gmail.com>
2019-10-21 13:38:02 -0400

    Implement a way of doing biomes as a trial [FAIL]

--- include/mapgen.hpp
+++ include/mapgen.hpp
@@ -6,6 +6,7 @@
 double* createMap(gen_attr attr);
 void normalizeMap(double*, int, int, double, double);
 int* populateBiomes(double* map, int, int);
+int moistureLevel(double* map, int i);
 void renderMap(int*, int, int);
 void saveMapPNG(SDL_Renderer*, int, int);
 void saveMap(double[], int, int);
--- src/mapgen.cpp
+++ src/mapgen.cpp
@@ -9,14 +9,6 @@
 #include "../include/mapgen.hpp"
 #include "../include/perlin.hpp"
 
-    //grayscale needs [0, 1]
-    //perlVal = (perlVal + 1) / 2;
-    //For grayscale
-    //int b = (int)(perlVal * 0xFF);
-    //int g = b * 0x100;
-    //int r = b * 0x10000;
-    //finalVal = r + g + b;
-
 
 using namespace std;
 
@@ -33,8 +25,7 @@ void renderMap(int* pixels, int width, int height) {
 	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, width, height);
 
   SDL_UpdateTexture(texture, NULL, pixels, width * sizeof(int));
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderClear(renderer); SDL_RenderCopy(renderer, texture, NULL, NULL);
   SDL_RenderPresent(renderer);
 
   //save png
@@ -56,7 +47,7 @@ void renderMap(int* pixels, int width, int height) {
   SDL_DestroyWindow(window);
   printf("Program ended\n");
   SDL_Quit();
-}
+} 
 
 double* createMap(gen_attr attr) {
   int space = attr.width * attr.height; 
@@ -73,8 +64,8 @@ double* createMap(gen_attr attr) {
   
   generateMap(attr, map);
 
-  double max = -1;
-  double min = 1;
+  double max = -10000;
+  double min = 10000;
   for(int i = 0; i < space; i++) {
     if(map[i] > max) max = map[i];
     if(map[i] < min) min = map[i];
@@ -109,53 +100,109 @@ int* populateBiomes(double* map, int width, int height) {
   int rocky = 0x796342;
   int mountainous = 0x9e9e9e;
   int snowy = 0xf0f0ec;
+  int savannah_green = 0x739a6d;
+  int desert_yellow = 0xca8d16; 
+  int taiga_green = 0x4a4016;
+  int tundra = 0xa3bfcb;
+  int tropical_green = 0x00755e;
+  int rainforest_green = 0x204020;
+  int grassland_green = 0x578054;
+
+  gen_attr mois_attr = {
+    .persistance = 0.5,
+    .lacunarity = 2,
+    .frequency = 32,
+    .octaves = 1,
+    .width = width,
+    .height = height,
+    .scale = width };
+
+  double* moisture_map = createMap(mois_attr);
+  double* temp_map = new double[space];
+
+  double max = -10000;
+  double min = 10000;
+  for(int i = 0; i < space; i++) {
+    temp_map[i] = (map[i] * map[i]) + (0.1 * moisture_map[i]);
+    if(temp_map[i] > max) max = temp_map[i];
+    else if(temp_map[i] < min) min = temp_map[i];
+  }
+
+  printf("temp min: %f, max: %f\n", min, max);
+
+  //normalizeMap(temp_map, width, height, min, max);
 
   double maxValue = 1.0f;
   
   for(int i = 0; i < space; i++) {
     int finalVal = 0;
 
+    int m_lvl = moistureLevel(temp_map, i);
+
+    //Mappings for biomes based loosely on the 
+    //Whittaker biome diagram
     if(map[i] < 0.32 * maxValue) finalVal = deep_ocean;
     else if(map[i] < 0.46 * maxValue) finalVal = sea_water;
     else if(map[i] < 0.5 * maxValue) finalVal = coastal_water;
     else if(map[i] < 0.51 * maxValue) finalVal = beach_sand;
-    else if(map[i] < 0.53 * maxValue) finalVal = inner_sand;
-    else if(map[i] < 0.65 * maxValue) finalVal = lowland_green;
-    else if(map[i] < 0.76 * maxValue) finalVal = midland_green;
-    else if(map[i] < 0.79 * maxValue) finalVal = low_rocky;
-    else if(map[i] < 0.82 * maxValue) finalVal = rocky;
-    else if(map[i] < 0.88 * maxValue) finalVal = mountainous;
-    else if(map[i] < 1.0 * maxValue) finalVal = snowy;
+    else if(map[i] < 0.7 * maxValue) {
+      switch(m_lvl) {
+        case 4: //HIGH TEMP, LOW ELEVATION
+          finalVal = tropical_green; 
+          break;
+        case 3: //MODERATE TO HIGH TEMPERATURE, LOW ELEV
+          finalVal = rainforest_green;
+          break;
+        default: finalVal = grassland_green;  
+      }
+    }
+    else if(map[i] < 0.8) {
+      switch(m_lvl) {
+        case 4: //High temp
+          finalVal = savannah_green;
+          break;
+        case 3:
+          finalVal = grassland_green;
+          break;
+        case 2:
+          finalVal = taiga_green;
+          break;
+        case 1:
+          finalVal = tundra;
+          break;
+      }
+    }
+    else if(map[i] < 0.9) {
+      switch(m_lvl) {
+        case 4: //High temp
+          finalVal = desert_yellow;
+          break;
+        case 3:
+          finalVal = savannah_green;
+          break;
+        case 2:
+          finalVal = taiga_green;
+          break;
+        case 1:
+          finalVal = tundra;
+          break;
+      }
+    }
+    else if(map[i] <= 1.0) {
+      finalVal = mountainous;
+    }
 
     pixels[i] = finalVal;
   }
   return pixels;
 }
 
-int main(int argc, char** argv) {
-	int width = 1200;
-	int height = 720;
-
-  gen_attr attr = {
-    .persistance = 0.6,
-    .lacunarity = 2,
-    .frequency = 3,
-    .octaves = 8,
-    .width = width,
-    .height = height,
-    .scale = width };
-
-  double* map = createMap(attr);
-
-  saveMap(map, width, height);
-
-  int* finalMap = populateBiomes(map, width, height);
-
-  renderMap(finalMap, width, height);
-
-  delete[] map;
-  delete[] finalMap;
-  return 0;
+int moistureLevel(double* map, int i) {
+  if(map[i] < 0.25) return 1;  //Low moisture
+  else if(map[i] < 0.55) return 2;
+  else if(map[i] < 0.9) return 3;
+  else if(map[i] <= 1.0) return 4;
+  else return -1;
 }
 
 void saveMapPNG(SDL_Renderer* renderer, int width, int height) {
@@ -207,4 +254,28 @@ void saveMap(double points[], int width, int height) {
   mapVals = NULL;
 }
 
+int main(int argc, char** argv) {
+	int width = 1200;
+	int height = 720;
 
+  gen_attr attr = {
+    .persistance = 0.6,
+    .lacunarity = 2,
+    .frequency = 3,
+    .octaves = 8,
+    .width = width,
+    .height = height,
+    .scale = 1.5 * width };
+
+  double* map = createMap(attr);
+
+  saveMap(map, width, height);
+
+  int* finalMap = populateBiomes(map, width, height);
+
+  renderMap(finalMap, width, height);
+
+  delete[] map;
+  delete[] finalMap;
+  return 0;
+}
